99fe8e1a4994be0317592834ffa5ad1811eea1be
BenjaminMeneses.dev <menesesvasquezb2004@gmail.com>
2026-08-21 01:46:43 -0400

    refactor: mueve HomePage a pages/home/

---
+++ fabull-final/src/pages/home/HomePage.jsx
@@ -0,0 +1,21 @@
+import Hero from '../../components/Hero'
+import Ticker from '../../components/Ticker'
+import Services from '../../components/Services'
+import About from '../../components/About'
+import Fleet from '../../components/Fleet'
+import Reviews from '../../components/Reviews'
+import Contact from '../../components/Contact'
+
+export default function HomePage() {
+  return (
+    <main>
+      <Hero />
+      <Ticker />
+      <Services />
+      <About />
+      <Fleet />
+      <Reviews />
+      <Contact />
+    </main>
+  )
+}
