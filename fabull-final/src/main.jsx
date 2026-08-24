7883cda86928e7efb1c9ec50e096980683896764
BenjaminMeneses.dev <menesesvasquezb2004@gmail.com>
2026-08-24 09:11:08 -0400

    refactor: actualiza imports de CSS a la nueva carpeta styles/

--- fabull-final/src/main.jsx
+++ fabull-final/src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
-import './fleet-carousel.css'
-import './responsive.css'
+import './styles/fleet-carousel.css'
+import './styles/responsive.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
