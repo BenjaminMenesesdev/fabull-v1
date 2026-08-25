f697cb9c56c441f82e11085f12c2b1b59f8be883
BenjaminMeneses.dev <menesesvasquezb2004@gmail.com>
2026-08-24 20:02:07 -0400

    fix: agregar ScrollToTop para reiniciar scroll al cambiar de pagina

--- fabull-final/src/App.jsx
+++ fabull-final/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import ScrollToTop from './components/ScrollToTop'
 import HomePage from './pages/home/HomePage'
 import ContactoPage from './pages/contacto/ContactoPage'
 import NosotrosPage from './pages/nosotros/NosotrosPage'
@@ -15,6 +16,7 @@ export default function App() {
   return (
     <ToastProvider>
       <BrowserRouter>
+        <ScrollToTop />
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
